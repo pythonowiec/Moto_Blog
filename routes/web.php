a7e72c5c1455803c386617e0fdec44a1bb61048b
pythonowiec <45623593+pythonowiec@users.noreply.github.com>
2021-11-19 12:53:58 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,9 @@ Auth::routes();
 Route::get('/dodaj_post', [PostsController::class, 'create'])->name('add')->middleware('auth');
 Route::post('/add', [PostsController::class, 'store']);
 
-
+Route::get('/autor', function () {
+    return view('author');
+})->name('author');
 
 Route::get('/o_blogu', function () {
     return view('info');
